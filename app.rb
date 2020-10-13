d98fbf19897a7161c7594799536137895c3470fb
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-13 15:28:40 +0100

    Returning HTML - Adding image with a boarder

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 get '/' do
   "Hello!"
 end
@@ -6,3 +7,18 @@ end
 get '/secret' do
   "Secret squirrle"
 end
+
+get '/code' do
+  "I wish I knew how to code"
+end
+
+get '/another_path' do
+  "Another page"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE '>
+  </div>"
+end 
+
